932f3c2d04127cb8f6faf7b59365e5fe6629ae16
Ashu261101 <Ashu261120001@gmail.com>
2023-10-03 10:07:09 +0530

    bgcolor

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('My code')
+
+//var header=document.getElementById('main-header')
+//header.style.borderBottom='solid 3px #000'
+var itemss=document.getElementsByClassName("list-group-item")
+console.log(itemss)
+console.log(itemss[1])
+itemss[1].style.fontWeight='bold'
+itemss[1].style.backgroundColor='yellow'
