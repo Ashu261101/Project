2fbb51a5d05972c2463d4950f62d7f1ae80872db
Ashu261101 <Ashu261120001@gmail.com>
2023-10-03 10:21:36 +0530

    bold changes

--- app.js
+++ app.js
@@ -2,7 +2,14 @@
 //var header=document.getElementById('main-header')
 //header.style.borderBottom='solid 3px #000'
 var itemss=document.getElementsByClassName("list-group-item")
-console.log(itemss)
-console.log(itemss[1])
-itemss[1].style.fontWeight='bold'
-itemss[1].style.backgroundColor='yellow'
+//console.log(itemss)
+//console.log(itemss[1])
+//itemss[1].style.fontWeight='bold'
+//itemss[2].style.backgroundColor='green'
+
+for(var i=0;i<itemss.length;i++)
+{
+    itemss[i].style.fontWeight='bold';
+}
+
+itemss[2].style.backgroundColor='green'
